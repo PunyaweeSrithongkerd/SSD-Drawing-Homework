1ca8ca8e090ff3171ecc1e92f9ad74e1a3e31207
Punyawee Srithonhkerd <tonkla9874@gmail.com>
2020-04-10 11:02:58 +0700

    finish the CompositeGOject and Rect

--- src/objects/CompositeGObject.java
+++ src/objects/CompositeGObject.java
@@ -15,30 +15,55 @@ public class CompositeGObject extends GObject {
 	}
 
 	public void add(GObject gObject) {
-		// TODO: Implement this method.
+		gObjects.add(gObject);
 	}
 
 	public void remove(GObject gObject) {
-		// TODO: Implement this method.
+		gObjects.remove(gObject);
 	}
 
 	@Override
 	public void move(int dX, int dY) {
-		// TODO: Implement this method.
+		if (this.selected) {
+			this.x += dX;
+			this.y += dY;
+		}
 	}
 	
 	public void recalculateRegion() {
-		// TODO: Implement this method.
+		int left_x = gObjects.get(0).x;
+		int right_x = gObjects.get(0).x;
+		int upper_y = gObjects.get(0).y;
+		int lower_y = gObjects.get(0).y;
+		for (int i = 0; i<gObjects.size()-1;i++){
+			if (left_x > gObjects.get(i).x){
+				left_x = gObjects.get(i).x;
+			}
+			if (right_x < gObjects.get(i).x){
+				right_x = gObjects.get(i).x;
+			}
+			if (upper_y < gObjects.get(i).y){
+				upper_y = gObjects.get(i).y;
+			}
+			if (lower_y > gObjects.get(i).y){
+				lower_y = gObjects.get(i).y;
+		}
+	}
+		this.x = left_x;
+		this.y = upper_y;
+		this.width = left_x + right_x;
+		this.height = upper_y + lower_y;
 	}
 
 	@Override
 	public void paintObject(Graphics g) {
-		// TODO: Implement this method.
+		for (GObject gobject: gObjects){
+			gobject.paintObject(g);
+		}
 	}
 
 	@Override
 	public void paintLabel(Graphics g) {
-		// TODO: Implement this method.
+		g.drawString("Group",this.x,this.y-this.height);
 	}
-	
 }
